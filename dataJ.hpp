2236eb061edfcc4bd30c79df8bb1b54eb4ef9acd
lucas SCHULZE <lucas.schulze03@gmail.com>
2025-03-28 09:46:57 +0100

    fichier data J

---
+++ dataJ.hpp
@@ -0,0 +1,7 @@
+double Mj=1.8986*pow(10,27);
+double mIo= 8.93*pow(10,22);
+double mEu= 4.8*pow(10,22);
+double mGa= 1.4819*pow(10,23);
+double aIo= 421800*pow(10,3);
+double aEu= 671100*pow(10,3);
+double aGa= 1070000*pow(10,3);
