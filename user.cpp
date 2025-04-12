905bc13e4ec0a2f87220fee668cf844b9ae12536
lucas SCHULZE <lucas.schulze03@gmail.com>
2025-04-12 12:25:42 +0200

    Stage 2

---
+++ user.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+double T;
+double a;
+double M;
+double m;
+
+    cout << "Masse principale (kg) : ";
+    cin >> M;
+    cout << "Masse corps orbiteur (kg) : ";
+    cin >> m;
+    cout << "Demi-grand axe (m) : ";
+    cin >> a;
+
